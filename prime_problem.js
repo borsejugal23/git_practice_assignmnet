70290a33f938392c0b25f65761fdab288a198a1b
divyasapkal <115499336+divyasapkal@users.noreply.github.com>
2022-10-20 00:11:15 +0530

    Update prime_problem.js

--- prime_problem.js
+++ prime_problem.js
@@ -1,12 +1,12 @@
 let number = 13;
 let count=0;
-for (i=0;i<=13-1;i++){
+for (i=0;i<=13;i++){
     if (number%i==0){
         count++;
         
 
     }
-}if (count>=2){
+}if (count==2){
     console.log("it's a prime number");
 }else{
     console.log("it not a prime number")
