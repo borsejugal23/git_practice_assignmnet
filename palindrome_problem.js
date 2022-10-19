29c81bfc1d9f45b79df7c5b6be1acd65249b1286
borsejugal <borsejugal@gmail.com>
2022-10-19 23:44:45 +0530

    added prime_number and palindrome problem

---
+++ palindrome_problem.js
@@ -0,0 +1,16 @@
+function checkPalindrome(N, str) {
+   
+    let = nstr="";
+     for(i=N-1;i>=0;i--){
+         nstr+=str[i];
+         
+     }
+     if(nstr==str){
+         console.log("No");
+     }
+    else{
+        console.log("Yes");
+    }
+    
+  
+}
